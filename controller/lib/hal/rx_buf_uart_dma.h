52b41fc9dc675ef92f90cad1a4adb0aa06391ef0
miceuz <mic@hardcore.lt>
2020-06-01 18:48:53 +0300

    Shuffle around class members to standard ordering

--- controller/lib/hal/rx_buf_uart_dma.h
+++ controller/lib/hal/rx_buf_uart_dma.h
@@ -7,46 +7,44 @@
 // We need this class to abstract hardware access from FrameDetector FSM so it
 // can be used in GUI code
 template <int RX_BYTES_MAX> class RxBufferUartDma {
-  UART_DMA &uart_dma;
-  uint8_t rx_buf_[RX_BYTES_MAX];
+public:
+  RxBufferUartDma(UART_DMA &uart_dma) : uart_dma_(uart_dma){};
 
+  // Sets up underlying receive infrastructure and starts the first reception
+  void RxBufferUartDma::Begin(RxListener *rxl) {
+    uart_dma_.charMatchEnable();
+    uart_dma_.startRX(rx_buf_, RX_BYTES_MAX, RX_TIMEOUT_, rxl);
+  }
+
+  // Restarts the ongoing reception, this means the rx_buf will be written from
+  // the beginning
+  void RestartRX(RxListener *rxl) {
+    uart_dma_.stopRX();
+    uart_dma_.startRX(rx_buf_, RX_BYTES_MAX, RX_TIMEOUT_, rxl);
+  }
+
+  // Returns how many bytes were written into rx_buf
+  uint32_t ReceivedLength() {
+    return (RX_BYTES_MAX - uart_dma_.getRxBytesLeft());
+  }
+
+  // Returns the rx_buf
+  uint8_t *get() { return rx_buf_; }
+
+#ifdef TEST_MODE
+  // Puts a byte to rx_buf
+  void test_PutByte(uint8_t b) { rx_buf_[rx_i++] = b; }
+#endif
+
+private:
+  // UART_DMA provides receiving to this buffer
+  UART_DMA &uart_dma_;
+  // Buffer into which the data is received
+  uint8_t rx_buf_[RX_BYTES_MAX];
   // If no activity is happening on RX line for the given amount of time,
   // listener will get an onError callback.
   // TODO determine the right amount of time via risk analysis
   static constexpr Duration RX_TIMEOUT_ = seconds(10);
-
-public:
-  RxBufferUartDma(UART_DMA &uart_dma) : uart_dma(uart_dma){};
-  void Begin(RxListener *);
-  void RestartRX(RxListener *);
-  uint32_t ReceivedLength();
-  uint8_t *get() { return rx_buf_; }
-#ifdef TEST_MODE
-  void test_PutByte(uint8_t b);
-#endif
 };
-
-template <int RX_BYTES_MAX>
-void RxBufferUartDma<RX_BYTES_MAX>::Begin(RxListener *rxl) {
-  uart_dma.charMatchEnable();
-  uart_dma.startRX(rx_buf_, RX_BYTES_MAX, RX_TIMEOUT_, rxl);
-}
-template <int RX_BYTES_MAX>
-void RxBufferUartDma<RX_BYTES_MAX>::RestartRX(RxListener *rxl) {
-  uart_dma.stopRX();
-  uart_dma.startRX(rx_buf_, RX_BYTES_MAX, RX_TIMEOUT_, rxl);
-}
-template <int RX_BYTES_MAX>
-uint32_t RxBufferUartDma<RX_BYTES_MAX>::ReceivedLength() {
-  return (RX_BYTES_MAX - uart_dma.getRxBytesLeft());
-}
-
-#ifdef TEST_MODE
-extern uint32_t rx_i;
-template <int RX_BYTES_MAX>
-void RxBufferUartDma<RX_BYTES_MAX>::test_PutByte(uint8_t b) {
-  rx_buf_[rx_i++] = b;
-}
-#endif
 
 #endif
